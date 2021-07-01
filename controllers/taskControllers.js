304345d3fc5c3a2d9a1d3f7b2527976b2ecf5baf
Arnau-Mas <arnau.mava@gmail.com>
2021-07-01 20:05:33 +0200

    fetch funcionando

--- controllers/taskControllers.js
+++ controllers/taskControllers.js
@@ -5,5 +5,12 @@ exports.completedTask = async (req, res) => {
 }
 
 exports.updateTask = async (req, res) => {
-    res.send(req.body);
+    console.log("*********", req.body);
+    const {idNote, titleModified, parafModified} = req.body;
+    // find de la nota a la db con idNote
+    console.log("*************", idNote);
+    if(idNote){
+    const taskUpdated = await workspaceModel.findById(idNote);
+    console.log(taskUpdated);
+    }   
 }
