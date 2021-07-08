80a999d25c27b8317e717b8652c6ebab8fc10ea9
Arnau-Mas <arnau.mava@gmail.com>
2021-07-08 21:02:11 +0200

    hideCompletedTask primera parte

--- models/workspaceModel.js
+++ models/workspaceModel.js
@@ -11,7 +11,7 @@ const taskSchema = new Schema({
 
 // Cada Espacio de Trabajo tiene una ID única y tiene incrustadas sus Notas asociadas.
 const workspaceSchema = new Schema({
-    tasks : [taskSchema], 
+    tasks : [taskSchema],
 });
 
 /* Asociamos la Colección con el Schema */
