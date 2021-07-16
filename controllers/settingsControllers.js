6c8b2740848bd9a92bb2f10eb25e66f809dec307
josecastp <chacoper1@hotmail.com>
2021-07-16 20:54:07 +0200

    eliminado formato CSS

--- controllers/settingsControllers.js
+++ controllers/settingsControllers.js
@@ -1,5 +1,6 @@
 const workspaceModel = require("../models/workspaceModel");
 const {validationResult} = require('express-validator');
+const exportFromJSON = require('export-from-json');
 
 exports.renderSettings = async (req,res) =>{
 
@@ -38,5 +39,57 @@ exports.updateSettings = async (req,res) =>{
     await workspace.save();
     console.log("actualizado", workspace); 
   }
+
+  
+  exports.exportTasks = async (req, res) =>{
+    const idWorkspace = req.query.idWorkspace; // id del workSapace
+    const exportType = req.query.exportTasks;  //Recuperamos del req mediante query, tipo de FORMATO SOLICITADO.
+    const allTasks = await workspaceModel.getAllTasks(idWorkspace); //Recuperamos todas las TAREAS de este workSpace en formA de ARRAY DE OBJETOS (BSONS, son los objetos que utiliza MongoDb)
+
+    //Si exportType es 'json', los datos pueden ser cualquier JSON parseable. Si exportType es 'csv' o 'xls', los datos solo pueden ser una matriz de JSON parseable. Si exportType es 'txt', 'css', 'html', los datos deben ser un tipo de cadena.
+    let data; //Declaramos 'data' para condicionarla SEGUN FORMATO DESEADO...
+    if (exportType == 'csv' || 'xls' ){
+      data = allTasks;
+
+    }else if(exportType == 'json'){
+      data  = JSON.stringify(allTasks);
+
+    }else{
+      data = JSON.stringify(allTasks);   
+      }
+    
+    const fileName = 'workSpaceFromMongoDb'
+    const result = await exportFromJSON({
+        data,
+        fileName,
+        exportType,
+        processor (content, type, fileName) {
+            switch (type) {
+                case 'txt':
+                    res.setHeader('Content-Type', 'text/plain')
+                    break
+                case 'html':
+                    res.setHeader('Content-Type', 'text/html')
+                    break
+                case 'json':
+                    res.setHeader('Content-Type', 'text/plain')
+                    break
+                case 'csv':
+                    res.setHeader('Content-Type', 'text/csv')
+                    break
+                case 'xls':
+                    res.setHeader('Content-Type', 'application/vnd.ms-excel')
+                    break
+            }
+            res.setHeader('Content-disposition', 'attachment;filename=' + fileName)
+            return content
+        }
+    })
+    res.write(result)
+    res.end()
+  }
+
+
+
 
 
--- routes/settingsRoutes.js
+++ routes/settingsRoutes.js
@@ -4,6 +4,8 @@ var router = express.Router();
 
 /* router.get("/")  */
 
+router.get("/export", settingsControllers.exportTasks); //Ha de estar por encima de los otros endpoints
+
 router.get("/:idWorkspace", settingsControllers.renderSettings);
 
 router.post("/updateSettings", settingsControllers.updateSettings);
