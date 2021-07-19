af4a6fd33436cd54959f5db735670ab1d7434126
Arnau-Mas <arnau.mava@gmail.com>
2021-07-19 21:13:53 +0200

    update

--- public/js/taskUpdate.js
+++ public/js/taskUpdate.js
@@ -13,7 +13,14 @@ function onFocusUpdate(event){
   let reminderDate = event.currentTarget.querySelector(`#reminder-date-${idTask}`).value;
   let reminderHour = event.currentTarget.querySelector(`#reminder-hour-${idTask}`).value;
   let data = {idTask, titleModified, textModified, reminderDate, reminderHour};
-  
+  console.log("****", event);
+  // LO DE AQUI ABAJO ES PARA AÑADIR LA FECHA AL MOMENTO, HABRÍA QUE CONSTRUIRLO A MANO. No sé si es buena idea o si hacer un res.render en el post del fetch
+/*   let reminderTag = document.querySelector(`#reminderTag-${idTask}`);
+  let reminderTagText = event.currentTarget.querySelector(`#reminderTagText-${idTask}`)
+  if(reminderDate && reminderHour){
+  reminderTag.removeAttribute("hidden");
+  reminderTagText.innerText = "b";
+  } */
   if(!titleModified && !textModified){
     console.log("Camoos text y title modified---------->",titleModified, textModified);
     //alert("Has de rellenar uno de los dos campos")
