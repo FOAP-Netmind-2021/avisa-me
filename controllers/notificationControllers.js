0bb83f7e3e6dc0612fcfdca5f4b61dea553d56ff
Arnau-Mas <arnau.mava@gmail.com>
2021-07-21 13:13:11 +0200

    building reminder nodemailer

--- controllers/notificationControllers.js
+++ controllers/notificationControllers.js
@@ -3,15 +3,17 @@ var nodemailer = require("../utils/nodemailer");
 
 exports.reminderNotification = async (req,res) =>{
   console.log("ha llegado")
-    const allTasks = await taskModel.find();
-    let dateTasks = allTasks.map(function(task){
-      return [task.reminderDate, task.reminderNotification, task._id]})
-    
-    let filteredDateTasks = dateTasks.filter(task=> task[0]>new Date());
-
-    /* console.log(tasks); */
-    //hacer el update de tasks para poner una fecha en el campo notification
+  let date = new Date();
+  let limitDate = new Date();
+  //falta setear bien el abanico de fechas porque ahora mismo no tengo claro si es las que faltan 8 horas o que x)
+  limitDate.setHours(limitDate.getHours()+8)
+  const allTasks = await taskModel.find({reminderDate:{$gte:date, $lt:limitDate}, reminderNotification:null}); //configurar el $lt por dia y hora
+  console.log(allTasks);
+  console.log(date);
+  console.log(limitDate);
 
   /* await nodemailer.sendEmail("prueba", "Aviso notificación", "arnau.mava@gmail.com") */
+
+  // Hacer el update de task para pasar el campo reminderNotification a new Date
   res.send("done");
 }
