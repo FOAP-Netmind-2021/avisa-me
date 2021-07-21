41e3fee3051b76f530d3bca58a27b5a40956a0f2
Arnau-Mas <arnau.mava@gmail.com>
2021-07-21 21:11:56 +0200

    push before global merge

--- controllers/notificationControllers.js
+++ controllers/notificationControllers.js
@@ -9,18 +9,15 @@ exports.reminderNotification = async (req,res) =>{
   let limitDate = new Date();
   limitDate.setHours(limitDate.getHours()+8)
   limitDate.setMinutes(limitDate.getMinutes()+5)
-  const allTasks = await taskModel.find({reminderDate:{$gt:initDate, $lt:limitDate}, reminderNotification:null}).populate("workspace"); //configurar el $lt por dia y hora
-  console.log(allTasks);
-  console.log(initDate);
-  console.log(limitDate);
-
+  const allTasks = await taskModel.find({reminderDate:{$gt:initDate, $lt:limitDate}, reminderNotification:null}).populate("workspace"); //
+  //Habría que buscar solo las tareas cuyo workspace tiene solo un user
+  //Habría que conseguir los datos del user
+  
   allTasks.forEach(async task=> {
-    /* task.reminderNotification = new Date(); */
-    await nodemailer.sendEmail(notificationTemplate("prueba","prueba2","prueba3"), "Juan", "arnau.mava@gmail.com")
+    task.reminderNotification = new Date();
+    /* await nodemailer.sendEmail(notificationTemplate("prueba","prueba2","prueba3"), "Juan", "arnau.mava@gmail.com") */
   });
-
-  /* await nodemailer.sendEmail("prueba", "Aviso notificación", "arnau.mava@gmail.com") */
-
   // Hacer el update de task para pasar el campo reminderNotification a new Date
+  console.log(allTasks)
   res.send("done");
 }
