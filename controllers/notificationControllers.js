962f2d817a0c3e172c7e267c6a5919a0ee691264
Arnau-Mas <arnau.mava@gmail.com>
2021-07-22 09:40:01 +0200

    push before global merge & building reminderNotification

--- controllers/notificationControllers.js
+++ controllers/notificationControllers.js
@@ -1,6 +1,9 @@
 const taskModel = require("../models/taskModel");
+const subscriptionModel = require("../models/subscriptionModel");
 var nodemailer = require("../utils/nodemailer");
 let {notificationTemplate} = require("../utils/notificationTemplate");
+
+//Todo esto habrá que pasarlo a un script que se inicialice cada 5 minutos. Por el momento, se hace en un endpoint para poder centrarnos en su funcionalidad.
 
 exports.reminderNotification = async (req,res) =>{
   let initDate = new Date();
@@ -10,14 +13,19 @@ exports.reminderNotification = async (req,res) =>{
   limitDate.setHours(limitDate.getHours()+8)
   limitDate.setMinutes(limitDate.getMinutes()+5)
   const allTasks = await taskModel.find({reminderDate:{$gt:initDate, $lt:limitDate}, reminderNotification:null}).populate("workspace"); //
-  //Habría que buscar solo las tareas cuyo workspace tiene solo un user
-  //Habría que conseguir los datos del user
   
-  allTasks.forEach(async task=> {
-    task.reminderNotification = new Date();
-    /* await nodemailer.sendEmail(notificationTemplate("prueba","prueba2","prueba3"), "Juan", "arnau.mava@gmail.com") */
-  });
-  // Hacer el update de task para pasar el campo reminderNotification a new Date
-  console.log(allTasks)
+  //Habría que buscar solo las tareas cuyo workspace tiene un user.
+  /* const filteredTasks = allTasks.filter() */
+  console.log("****allTasks****", allTasks);
+  
+  allTasks.forEach(async task=>{
+    let id = task.workspace._id;
+    let subscriber = await subscriptionModel.find({workspaces:id}).populate("user")
+    let userEmail = subscriber.user.email;
+   /*  task.reminderNotification = new Date(); 
+    await nodemailer.sendEmail(notificationTemplate("prueba","prueba2","prueba3"), "Recordatorio: Faltan 8horas para tu tarea", userEmail)  */
+    // Hacer el update/save de task para pasar el campo reminderNotification a new Date
+  })   
+ 
   res.send("done");
 }
